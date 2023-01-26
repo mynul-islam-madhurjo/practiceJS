74c0747c969c710d502c42326830a2a382ed2e4e
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-01-26 11:52:51 +0600

    conditions

---
+++ conditions.js
@@ -0,0 +1,39 @@
+// Conditional Statements
+var x = 10;
+var y = 10;
+
+if (x == 10 && y == 20) {
+    // will not print
+    document.body.innerHTML= '10'
+}else if (false){
+    document.body.innerHTML= 'Not Working'
+}else{
+    document.body.innerHTML= 'Working'
+}
+
+
+
+// if (x == 10 || y == 20) {
+//     // print
+//     document.body.innerHTML= '10'
+// }else{
+//     document.body.innerHTML= 'Not Working'
+// }
+
+
+
+
+// if (x == 10){
+//     document.body.innerHTML= '10'
+// }else{
+//     document.body.innerHTML= 'Not Working'
+// }
+
+
+
+// if (true){
+//     document.body.innerHTML= 'Working'
+// }else{
+//     document.body.innerHTML= 'Not Working'
+// }
+
