00be5bbfaed507dd7fc11f35b88072510bad0868
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-01-26 09:59:09 +0600

    Data types and operators

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+// Ways to write string data type 
+
+//var a = 'My JS front page';
+//var a = 'Mynul\'s JS front page';
+var a = "Mynul's \''JS''\ front page";;
+// Numeric Data type
+var d = 10;
+var e = 10.9;
+// Boolean Data type
+var bool1 = true;
+var bool2 = false;
+var bool3 = 1;
+var bool3 = 0;
+
+// Undefined Data
+var b;
+
+// Null Data type
+var c = null;
+
+console.log(document.body.innerHTML = a);
+console.log(document.body.innerHTML = typeof a == 'string' ? 'mynul' : 'not found');
---
+++ operator.js
@@ -0,0 +1,26 @@
+// assignment operators
+/* 
+ = 
+ +=
+ -=
+ *=
+ /=
+ %=
+ */
+
+ // arithmatic operators
+/* 
++
+-
+*
+/
+ */
+
+ var a = 10;
+ //a = 10+5;
+ a += 5;
+ //console.log(document.body.innerHTML = a);
+
+ var b = 24;
+ var info = 'My age is ' + b + ' years old' ;
+ document.body.innerHTML = info;
