38fefea8f41c30435e82df19c46b65cd538770d4
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-01-26 12:04:18 +0600

    Arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+// How to declare arrays 
+var a = [1, 2, 3, 4];
+var b = [1, true, 'ok'];
+
+document.body.innerHTML= 'This is the array ' + a;
+document.body.innerHTML= 'This is the first element of array ' + a[1];
